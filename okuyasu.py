800c89804e2710291f810944c70b2996e4367b7e
DONUT235 <blahguy413@gmail.com>
2020-07-19 19:30:07 -0400

    first attempt at delete

--- okuyasu.py
+++ okuyasu.py
@@ -21,9 +21,32 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.lower().startswith('okuyasu'):
-        await message.channel.send(OKUYASU_HELP)
+    if is_okuyasu_command(message):
+        split_content = message.content.lower().split()
+        if len(split_content) < 2:
+            return
+        if split_content[1] == 'help':
+            await message.channel.send(OKUYASU_HELP)
+        elif split_content[1] == 'delete':
+            try:
+                num_to_delete = int(split_content[2])
+            except IndexError, ValueError:
+                pass
+            await delete_n_previous(message, num_to_delete)
 
+def is_okuyasu_command(message):
+    #TODO Check for user privileges
+    return message.content.lower().startswith('okuyasu')
+
+
+async def delete_n_previous(message, num_to_delete):
+    #TODO get channel of message, get the messages to delete with
+    #channel.history(limit=num_to_delete, before=message
+    channel = message.channel
+    async for prev_message 
+        in channel.history(before=message, limit=num_to_delete):
+            await prev_message.delete()
+    await channel.send("Gotcha!")
 
 if __name__ == '__main__':
     client.run(os.getenv("OKUYASU_TOKEN"))
