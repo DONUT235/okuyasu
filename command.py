302da0d56d05adcf2a7b5b96abfed9ddec31241b
DONUT235 <blahguy413@gmail.com>
2020-07-28 22:17:13 -0400

    more bugs

--- command.py
+++ command.py
@@ -87,7 +87,7 @@ class BanWordInDBCommand(NeedsGuildCommand):
             #This is potentially dangerous!
             return
         await db.ban_phrase(
-            server_id, phrase_to_ban, self.match_type.db_name)
+            str(server_id), phrase_to_ban, self.match_type.db_name)
 
         await message.channel.send(
             f'The phrase "{phrase_to_ban}" is now banned.')
