0dacc957b271e8bef22d095b09a4f95d76c56cad
Aaron Glave <glaveaf@rose-hulman.edu>
2021-01-12 21:47:33 -0500

    add permission table

---
+++ migrations/migration6.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Permissions (
+	discord_id character varying PRIMARY KEY,
+	can_kill boolean NOT NULL
+);
+
+INSERT INTO Permissions (discord_id, can_kill)
+SELECT DISTINCT (discord_id, FALSE) FROM banned_phrases;
